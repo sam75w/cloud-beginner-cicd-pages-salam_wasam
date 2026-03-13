23315dda33111c61b2811da163bc8b784e6160f1
sam75w <salam15wesbuh@gmail.com>
2026-03-13 08:56:30 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("changeTextBtn").addEventListener("click", function () {
+  document.getElementById("message").textContent =
+    "The button was clicked successfully. GitHub Actions will deploy this site automatically!";
+});
